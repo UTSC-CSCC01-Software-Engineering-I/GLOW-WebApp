ff6829dc192bf962d71150166648a86f5ce293bc
Shaaf Yousaf <muhdshaaf@gmail.com>
2025-06-14 08:01:50 -0400

    Minor Aesthetic Changes to UI
    Fetching alert now looks a lot better.

--- frontend/src/components/HUDloading.jsx
+++ frontend/src/components/HUDloading.jsx
@@ -6,14 +6,14 @@ import { useRouter } from 'next/navigation';
 function LoginBox({ theme, loading }) {
   const router = useRouter();
   return (      
-    <div style={{ backgroundColor: !loading ? 'rgba(24, 210, 58, 0.78)': 'rgba(210, 24, 24, 0.4)', width: '19rem', height: '3rem', top: '1rem',
+    <div style={{ backgroundColor: !loading ? 'rgba(24, 210, 58, 0.96)': 'rgba(210, 24, 24, 0.4)', width: '19rem', height: '3rem', top: '1rem',
       right: '14.7rem', position: 'fixed', borderRadius: '0.6rem', display: 'flex', 
       alignItems: 'center', justifyContent: 'left', flexDirection: 'row-reverse', 
       opacity: loading ? 1 : 0,
-      transition: 'opacity 8s ease',}}>
+      transition: 'opacity 5s ease-in'}}>
         
         <h1 style={{ 
-          color: theme === 'dark' ? 'white': 'black', 
+          color: 'white', 
           fontFamily: 'hubot sans', 
           fontWeight: '300', 
           fontSize: '1rem', 
@@ -24,10 +24,15 @@ function LoginBox({ theme, loading }) {
           cursor: 'pointer',
           width: 'auto',
           height: 'fit-content',
+          display:'flex',
+          flexDirection: 'row',
+          gap: '0.5rem',
+          alignItems: 'center'
+
           
           
         }}
-        >{loading ? 'Fetching latest data from api...' : 'Latest data loaded!'}</h1>
+        ><h1 style={{ fontSize: '1.4rem', marginBottom: '0.1rem' }}>🛈</h1>{loading ? 'Fetching latest data from api...' : 'Latest data loaded!'}</h1>
   </div>
     
   
