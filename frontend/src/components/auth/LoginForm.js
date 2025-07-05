0111d642b101f876df350d2c28e5601eb43a4cb3
Adrit Panday <145227719+Adrit-3001@users.noreply.github.com>
2025-07-05 00:11:56 -0400

    Revert "Feat/colorpoints"

--- frontend/src/components/auth/LoginForm.js
+++ frontend/src/components/auth/LoginForm.js
@@ -278,7 +278,7 @@ export default function LoginForm() {
           }}
           className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 mt-2"
         >
-          Don&apos;t have an account? Sign Up
+          Don't have an account? Sign Up
         </button>
 
         {message && (
